e9620471f65e112c2c0f08d18e6c76015acb26f2
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-11-26 17:32:19 +0200

    hw07

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PhoneBooks from './PhoneBooks/PhoneBooks';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <PhoneBooks />
+    </div>
+  );
+};
+
+export default App;
